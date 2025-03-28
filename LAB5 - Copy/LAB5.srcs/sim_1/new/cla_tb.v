efcdc64e28af3e6e928d20db8f20f5b8e04b8006
Kamsi-Elele <samuelelele209@gmail.com>
2025-03-27 22:22:41 -0500

    some part 2 things

---
+++ LAB5 - Copy/LAB5.srcs/sim_1/new/cla_tb.v
@@ -0,0 +1,64 @@
+`timescale 1ns / 1ps
+
+module cla_4bits_tb;
+
+    reg clk;
+    reg enable;
+    reg [3:0] A, B;
+    reg Cin;
+    wire [4:0] Q;
+
+    parameter period = 10;
+
+    CLA_4bits uut (
+        .clk(clk),
+        .enable(enable),
+        .A(A),
+        .B(B),
+        .Cin(Cin),
+        .Q(Q)
+    );
+
+    // Clock generation
+    initial begin
+        clk = 1;
+        forever #(period / 2) clk = ~clk;
+    end
+
+    // Test sequence
+    initial begin
+        enable = 0;
+        A = 0; B = 0;  Cin = 1'b0;;
+
+        @(negedge clk); 
+        A = 4'b0000; B = 4'b0101; Cin = 0; enable = 1;
+        @(negedge clk); enable = 0;
+
+        @(negedge clk); 
+        A = 4'b0101; B = 4'b0111; Cin = 0; enable = 1;
+        @(negedge clk); enable = 0;
+
+        @(negedge clk); 
+        A = 4'b1000; B = 4'b0111; Cin = 1; enable = 1;
+        @(negedge clk); enable = 0;
+
+        @(negedge clk); 
+        A = 4'b1001; B = 4'b0100; Cin = 0; enable = 1;
+        @(negedge clk); enable = 0;
+
+        @(negedge clk); 
+        A = 4'b1000; B = 4'b1000; Cin = 1; enable = 1;
+        @(negedge clk); enable = 0;
+
+        @(negedge clk); 
+        A = 4'b1101; B = 4'b1010; Cin = 1; enable = 1;
+        @(negedge clk); enable = 0;
+
+        @(negedge clk); 
+        A = 4'b1110; B = 4'b1111; Cin = 0; enable = 1;
+        @(negedge clk); enable = 0;
+
+        @(negedge clk); $finish;
+    end
+
+endmodule
--- LAB5 - Copy/LAB5.srcs/sources_1/imports/Lab5_Starter_Code/CLA.v
+++ LAB5 - Copy/LAB5.srcs/sources_1/imports/Lab5_Starter_Code/CLA.v
@@ -16,7 +16,56 @@ module CLA_4bits (
     // Assign initial carry-in
     assign C[0] = Cin;
 
-    // Module implementation goes here
+// propogate:
+//assign P = A ^ B;
 
+assign P[0] = A[0] ^ B[0];
+assign P[1] = A[1] ^ B[1];
+assign P[2] = A[2] ^ B[2];
+assign P[3] = A[3] ^ B[3];
+
+// Generate
+//assign G = A & B;
+assign G[0] = A[0] & B[0];
+assign G[1] = A[1] & B[1];
+assign G[2] = A[2] & B[2];
+assign G[3] = A[3] & B[3];
+
+//carry outs
+assign C[1]= G[0] | P[0]&C[0];
+assign C[2]= G[1] | P[1]&C[1];
+assign C[3]= G[2] | P[2]&C[2];
+assign C[4]= G[3] | P[3]&C[3];
+                
+// sum
+  assign S[0] = P[0] ^ C[0];
+  assign S[1] = P[1] ^ C[1];
+  assign S[2] = P[2] ^ C[2];
+  assign S[3] = P[3] ^ C[3];
+  
+  assign Cout = C[4];
+  assign Sum = S;
+  assign result = {C[4], Sum};  // MSB = carry out, LSB = sum
+
+    register_logic reg1 (.clk(clk), .enable(enable), .Data(result), .Q(Q));
 endmodule
+
+module register_logic (
+    input clk,
+    input enable,
+    input [4:0] Data,
+    output reg [4:0] Q
+);
+
+
+initial begin 
+    Q[4:0]  = 4'b0000; 
+    end
+    
+    always @ (posedge clk) begin
+        if (enable)
+            Q <= Data;
+    end
+endmodule
+
 
